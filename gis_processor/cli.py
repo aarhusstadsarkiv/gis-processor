4a3a73a39903a0ebf7c3a99fa51eababec152c8d
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-06-18 10:20:23 +0200

    gis_processor - rewrite using acacore 1.2.0

---
+++ gis_processor/cli.py
@@ -0,0 +1,195 @@
+from logging import Logger
+from os import PathLike
+from pathlib import Path
+from shutil import copy
+from sqlite3 import connect
+from sys import stdout
+from traceback import format_tb
+from uuid import UUID
+from uuid import uuid4
+
+from acacore.__version__ import __version__ as __acacore_version__
+from acacore.database import FileDB
+from acacore.models.file import File
+from acacore.models.history import HistoryEntry
+from acacore.models.reference_files import IgnoreAction
+from acacore.utils.helpers import ExceptionManager
+from acacore.utils.log import setup_logger
+from click import argument
+from click import command
+from click import Context
+from click import option
+from click import pass_context
+from click import Path as ClickPath
+from click import version_option
+
+from .__version__ import __version__
+from .processor import find_processor
+from .processor import Processor
+
+
+def handle_start(ctx: Context, database: FileDB, *loggers: Logger):
+    program_start: HistoryEntry = HistoryEntry.command_history(
+        ctx,
+        "start",
+        data=ctx.params | {"version": __version__, "acacore": __acacore_version__},
+    )
+
+    database.history.insert(program_start)
+
+    for logger in loggers:
+        logger.info(program_start.operation)
+
+
+def handle_end(ctx: Context, database: FileDB, exception: ExceptionManager, *loggers: Logger, commit: bool = True):
+    program_end: HistoryEntry = HistoryEntry.command_history(
+        ctx,
+        "end",
+        data=repr(exception.exception) if exception.exception else None,
+        reason="".join(format_tb(exception.traceback)) if exception.traceback else None,
+    )
+
+    for logger in loggers:
+        if exception.exception:
+            logger.error(f"{program_end.operation} {exception.exception!r}")
+        else:
+            logger.info(program_end.operation)
+
+    if database.is_open:
+        database.history.insert(program_end)
+        if commit:
+            database.commit()
+
+
+def file_not_found_error(
+    ctx: Context,
+    file_type: str,
+    path: str | PathLike,
+    uuid: UUID | None,
+    orig: dict | None,
+) -> HistoryEntry:
+    return HistoryEntry.command_history(ctx, f"file.{file_type}:error", uuid, orig, f"{path} not in database")
+
+
+def get_file(db: FileDB, path: str | PathLike) -> File | None:
+    return db.files.select(
+        where="relative_path = ?",
+        params=[str(Path(path))],
+        limit=1,
+    ).fetchone()
+
+
+@command("gis-processor")
+@argument("root", nargs=1, type=ClickPath(exists=True, file_okay=False, writable=True, resolve_path=True))
+@argument("avid", nargs=1, type=ClickPath(exists=True, dir_okay=False, readable=True, resolve_path=True))
+@option("--dry-run", is_flag=True, default=False, help="Show changes without committing them.")
+@version_option(__version__)
+@pass_context
+def app(ctx: Context, root: str | PathLike, avid: str | PathLike, dry_run: bool):
+    root, avid = Path(root), Path(avid)
+    database_path: Path = root / "_metadata" / "files.db"
+
+    if not database_path.is_file():
+        raise FileNotFoundError(database_path)
+
+    if not avid.is_file():
+        raise FileNotFoundError(avid)
+
+    program_name: str = ctx.find_root().command.name
+    logger: Logger = setup_logger(program_name, files=[database_path.parent / f"{program_name}.log"], streams=[stdout])
+
+    with connect(avid) as avid_conn:
+        if not (processor_cls := find_processor(avid_conn)):
+            raise ValueError(f"{avid!r} is not recognised")
+        processor: Processor = processor_cls(avid_conn)
+
+        with FileDB(database_path) as db:
+            handle_start(ctx, db, logger)
+
+            with ExceptionManager() as exception:
+                for main_file_orig in processor.find_main_files():
+                    main_file: File | None = get_file(db, p := processor.file_to_path(main_file_orig))
+
+                    if not main_file:
+                        error = file_not_found_error(ctx, "main", p, None, main_file_orig)
+                        logger.error(f"{error.operation} {error.reason}")
+                        continue
+                    elif (p := main_file.get_absolute_path(root)).exists():
+                        error = file_not_found_error(ctx, "main", p, main_file.uuid, None)
+                        logger.error(f"{error.operation} {main_file.uuid} {error.reason}")
+                        continue
+
+                    aux_files: list[tuple[File, File]] = []
+
+                    for aux_file_orig in processor.find_auxiliary_files(main_file_orig):
+                        aux_file: File | None = get_file(db, processor.file_to_path(aux_file_orig))
+
+                        if not aux_file:
+                            error = file_not_found_error(ctx, "aux", p, None, aux_file_orig)
+                            logger.error(f"{error.operation} {error.reason}")
+                            aux_files = []
+                            break
+                        elif (p := aux_file.get_absolute_path(root)).exists():
+                            error = file_not_found_error(ctx, "aux", p, aux_file.uuid, None)
+                            logger.error(f"{error.operation} {aux_file.uuid} {error.reason}")
+                            aux_files = []
+                            break
+
+                        if aux_file.action == "ignore":
+                            continue
+
+                        aux_file.action = "ignore"
+                        aux_file.action_data = IgnoreAction(reason="Auxiliary file")
+                        new_path: Path = main_file.relative_path.with_name(aux_file.name)
+
+                        aux_file_copy: File
+
+                        if aux_file_copy_ := get_file(db, new_path):
+                            if aux_file_copy_.checksum != aux_file.checksum:
+                                error = HistoryEntry.command_history(
+                                    ctx,
+                                    "file.aux:error",
+                                    None,
+                                    str(p),
+                                    f"{p} already exists with different hash",
+                                )
+                                logger.error(f"{error.operation} {error.reason}")
+                                aux_files = []
+                                break
+                            aux_file_copy = aux_file_copy_
+                        else:
+                            aux_file_copy = aux_file.model_copy(update={"uuid": uuid4(), "relative_path": new_path})
+                            if (p := aux_file_copy.get_absolute_path(root)).exists():
+                                error = HistoryEntry.command_history(
+                                    ctx,
+                                    "file.aux:error",
+                                    None,
+                                    str(p),
+                                    f"{p} already exists",
+                                )
+                                logger.error(f"{error.operation} {error.reason}")
+                                aux_files = []
+                                break
+
+                        aux_files.append((aux_file, aux_file_copy))
+
+                    for aux_file, aux_file_copy in aux_files:
+                        event = HistoryEntry.command_history(
+                            ctx, "file.aux:copy", aux_file_copy.uuid, aux_file_copy.relative_path
+                        )
+
+                        logger.info(f"{event.operation} {event.uuid} {event.data} Copy auxiliary file")
+
+                        if dry_run:
+                            continue
+
+                        try:
+                            copy(aux_file.get_absolute_path(root), aux_file_copy.get_absolute_path(root))
+                            db.files.insert(aux_file_copy, replace=True)
+                            db.files.update(aux_file)
+                            db.history.insert(event)
+                        except BaseException:
+                            aux_file_copy.get_absolute_path(root).unlink(missing_ok=True)
+                            raise
+
+            handle_end(ctx, db, exception, logger, commit=not dry_run)
---
+++ gis_processor/processor.py
@@ -0,0 +1,113 @@
+from abc import ABC
+from abc import abstractmethod
+from os import PathLike
+from pathlib import Path
+from sqlite3 import Connection
+from sqlite3 import Cursor
+from sqlite3 import Row
+from typing import Any
+from typing import Generator
+from typing import Type
+
+
+class Processor(ABC):
+    _main_extensions: list[str] = [
+        ".mif",
+        ".shp",
+        ".tab",
+    ]
+    _aux_extensions: dict[str, list[str]] = {
+        ".mif": [
+            ".mid",
+        ],
+        ".shp": [
+            ".aih",
+            ".ain",
+            ".atx",
+            ".cpg",
+            ".dbf",
+            ".fbn",
+            ".ixs",
+            ".mxs",
+            ".prj",
+            ".sbn",
+            ".sbx",
+            ".shx",
+            ".xml",
+        ],
+        ".tab": [
+            ".dat",
+            ".id",
+            ".ind",
+            ".map",
+        ],
+    }
+
+    def __init__(self, conn: Connection):
+        self.conn: Connection = conn
+
+    @property
+    def main_extensions(self):
+        return sorted(set(self._main_extensions), key=self._main_extensions.index)
+
+    @classmethod
+    @abstractmethod
+    def is_valid(cls, connection: Connection) -> bool: ...
+
+    @abstractmethod
+    def file_to_path(self, file: dict[str, Any]) -> str | PathLike: ...
+
+    @abstractmethod
+    def find_main_files(self) -> Generator[dict[str, Any], None, None]: ...
+
+    @abstractmethod
+    def find_auxiliary_files(self, main_file: dict[str, Any]) -> Generator[dict[str, Any], None, None]: ...
+
+
+# noinspection SqlNoDataSourceInspection,SqlResolve
+class CiriusNotesProcessor(Processor):
+    _tables: list[str] = ["dokument", "dokument_dokument", "dokument_fil", "fil", "sag", "sag_dokument"]
+
+    @classmethod
+    def is_valid(cls, connection: Connection) -> bool:
+        tables: list[str] = [
+            n.lower() for [n] in connection.execute("select name from sqlite_master where type = 'table'")
+        ]
+        if all(t in cls._tables for t in tables):
+            return True
+
+    def file_to_path(self, file: dict[str, Any]) -> str | PathLike:
+        doc_collection: int = int(file["doc_collection_id"])
+        file_id: int = int(file["fil_id"])
+        return Path(str(doc_collection), str(file_id), file["filename"])
+
+    def find_main_files(self) -> Generator[dict[str, Any], None, None]:
+        cursor: Cursor = self.conn.cursor()
+        cursor.execute(
+            f'select * from fil where {" or ".join(f"filename like '%' || ?" for _ in self.main_extensions)}',
+            self.main_extensions,
+        )
+        cursor.row_factory = Row
+        yield from (dict(f) for f in cursor)
+        cursor.close()
+
+    def find_auxiliary_files(self, main_file: dict[str, Any]) -> Generator[dict[str, Any], None, None]:
+        cursor: Cursor = self.conn.cursor()
+        cursor.execute(
+            "select * from file where notes_template_id = ?",
+            [main_file["notes_template_id"]],
+        )
+        cursor.row_factory = Row
+        yield from (
+            dict(f)
+            for f in cursor.fetchall()
+            if Path(f["filename"]).stem == Path(main_file["filename"]).stem
+            and Path(f["filename"]).suffix.lower()
+            in self._aux_extensions[Path(main_file["filename"]).suffix.lower()]
+        )
+        cursor.close()
+
+
+def find_processor(conn: Connection) -> Type[Processor] | None:
+    cls: Type[Processor]
+    return next((cls for cls in (CiriusNotesProcessor,) if cls.is_valid(conn)), None)
